1ca328ccec6e514ab95736a625a4c823f2b3a529
Mekylei Belchior <mekylei@msn.com>
2026-05-22 13:09:17 -0300

    feat: criado a base do projeto

---
+++ src/test/java/com/mekylei/transactionprocessing/TransactionProcessingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mekylei.transactionprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionProcessingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
